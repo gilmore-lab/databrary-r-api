9f0bce20a7df7286035170d51009264ae195b274
rogilmore <thatrickgilmore@gmail.com>
2015-03-26 07:30:21 -0400

    add logout

--- databrary.logout.R
+++ databrary.logout.R
@@ -4,13 +4,15 @@
 #----------------------------------------------------------
 
 databrary.logout <- function(logout.url="/api/user/logout", return.response=FALSE){
-  require(httr)
-  databrary.url <- "https://nyu.databrary.org"
-  set_config(add_headers(.headers = c("X-Requested-With" = "databrary R client")))
-    
+
+  if (!databrary.config.status){
+    source("databrary.config.R")
+    databrary.config()
+  }
+  
   r <- POST( paste(databrary.url, logout.url, sep = ""))
   
-  if ( status_code(r) == 200 ){
+  if (status_code(r) == 200){
     cat( 'Logout Successful.\n' )
     if (file.exists(".databrary.RData")) file.remove(".databrary.RData")
   } else cat( paste('Logout Failed, HTTP status ', status_code(r), '\n', sep="" ))
